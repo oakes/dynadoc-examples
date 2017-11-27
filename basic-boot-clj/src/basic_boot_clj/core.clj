808cef3cf1b7cdef58cf56833ff04a653f9fe16c
oakes <zach@sekao.net>
2017-11-26 21:02:58 -0500

    Add conj examples

--- basic-boot-clj/src/basic_boot_clj/core.clj
+++ basic-boot-clj/src/basic_boot_clj/core.clj
@@ -1,6 +1,6 @@
 (ns basic-boot-cljs.core
   (:require [clojure.string :as str]
-            [dynadoc.example :refer [defexample]]))
+            [dynadoc.example :refer [defexample defexamples]]))
 
 (defn get-extension
   "Returns the extension in the given path name."
@@ -12,4 +12,12 @@
 
 (defexample get-extension
   (get-extension "myfile.txt"))
+
+(defexamples clojure.core/conj
+  ["Add a name to a vector"
+   (conj ["Alice" "Bob"] "Charlie")]
+  ["Add a number to a list"
+   (conj '(2 3) 1)]
+  ["Add a key-val pair to a hash map"
+   (conj {:name "Alice"} [:age 30])])
 
